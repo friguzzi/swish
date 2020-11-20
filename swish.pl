d6e64299b963fa90ab7bee17f66f2267cd0cb597
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2020-11-20 17:26:13 +0000

    viterbi module loading

--- swish.pl
+++ swish.pl
@@ -285,6 +285,7 @@ pengines:prepare_module(Module, swish, _Options) :-
 :- use_module(library(lemur)).
 :- use_module(library(auc)).
 :- use_module(library(matrix)).
+:- use_module(library(viterbi)).
 
 
 :- use_module(swish(lib/render/gvterm),   []).
